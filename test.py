62c080f32f0adb3c3a5d5dd2dd1006bf9f4ed4c8
Moltastro <37265606+Moltastro@users.noreply.github.com>
2025-10-17 18:53:19 +0200

    Working on different render pipeline

--- test.py
+++ test.py
@@ -1,60 +1,102 @@
-"""
-from RevisedMicroGame.gameHandler import *
-from RevisedMicroGame.displayInterface import DebugDisplay
-from RevisedMicroGame.tileManager import GridTileManager
-from RevisedMicroGame.gameObject import GameObject
-from RevisedMicroGame.util import rgb
-from RevisedMicroGame.drawable import Rectangle
-import time 
-"""
-from lib.RevisedMicroGame.gameHandler import *
-from lib.RevisedMicroGame.displayInterface import DebugDisplay
-from lib.RevisedMicroGame.tileManager import GridTileManager
-from lib.RevisedMicroGame.gameObject import GameObject
-from lib.RevisedMicroGame.coordinateSystem import VectorMapFactory
-from lib.RevisedMicroGame.util import rgb
-from lib.RevisedMicroGame.drawable import Rectangle
-import time 
-debug=DebugDisplay(16,16)
-Game=GameHandler(ScriptScheduler(),GridTileManager(debug,2,2),VectorMapFactory())
-GameObject.bind_game_handler(Game)
-rect=GameObject(Rectangle(0,0,2,2))
-rect2=GameObject(Rectangle(x=3,y=1,w=1,h=1,color=rgb(233, 9, 9)))
-rect2.velocity=Vec2(-1,0)
-rect.velocity.x=1
-rect.velocity.y=1
-for i in range(20):
-    start = time.ticks_ms()
-    Game.update()
-    print("--------------Output:-----------------")
-    debug.display()
-    print("--------------------------------------")
-    elapsed = time.ticks_diff(time.ticks_ms(), start)
-    print(f"frame took {elapsed} ms")
-    time.sleep(0.5)
+from dataclasses import dataclass
+from typing import Protocol
+#from framebuff import FrameBuffer
+class Vec2:
+  def __init__(self,x,y):
+    self.x=x
+    self.y=y
+  def __add__(self,other):
+    return Vec2(self.x+other.x,self.y+other.y)
+  def __repr__(self):
+    return f"{self.x,self.y}"
+  
+class DrawCallable(Protocol):
+  def __call__(self, fb,pos)->None: ...
     
-""" 
-if __name__ == "__main__":
-    from RevisedMicroGame.coordinateSystem import Vec2
-    from RevisedMicroGame.displayInterface import DebugDisplay
-    from RevisedMicroGame.byteBuffer import ByteBuffer
-    import struct
+@dataclass(order=True)
+class RenderAction:
+  action:DrawCallable
+  z:int
 
-    dbg = DebugDisplay(8, 4)
+class DrawAction:
+  def __init__(self,offset,z=0):
+    self.offset=offset
+    self.z=z
 
-    # Fill a 4x2 subregion (window)
-    sub_buf = ByteBuffer(Vec2(4, 2))
-    for y in range(2):
-        for x in range(4):
-            r = int((x / 3) * 31)
-            g = int((y / 1) * 63)
-            b = 15
-            rgb565 = (r << 11) | (g << 5) | b
-            idx = (y * 4 + x) * 2
-            sub_buf.buffer[idx:idx+2] = struct.pack("<H", rgb565)
+  def build(self,WorldCoordinatePos) ->DrawCallable:
+    return RenderAction(lambda fb,pos:self.draw(self.offset+WorldCoordinatePos,fb),self.z)
 
-    dbg.set_window(BBox(Vec2(2, 1), Vec2(4, 2)))
-    dbg.send_color_data(sub_buf)
+  def draw(self, pos, fb:'ByteBuffer'):
+    raise NotImplementedError
+  
+  def __lt__(self,other:'DrawAction'):
+    return self.z<other.z
 
-    # Show entire display (with only that window filled)
-    dbg.display() """
+class Rectangle(DrawAction):
+  def __init__(self, offset, size, z=0):
+    super().__init__(offset, z)
+    self.size=size
+
+  def draw(self,pos,fb):
+    print(f"Drawing rectangle at {pos} with size {self.size}")
+
+    
+class Node:
+  def __init__(self,pos):
+    self.pos=pos
+    self.children=set()
+  def add_child(self,other:'Node'):
+    self.children.add(other)
+  def remove_child(self,other:'Node'):
+    self.children.discard(other)
+  def gatherDrawCalls(self,pos=Vec2(0,0),draw=[]):
+    child:'Node'
+    for child in self.children:
+      child.gatherDrawCalls(pos+self.pos,draw)
+
+class GameObject(Node):
+  def __init__(self, pos,sprite:DrawAction):
+    super().__init__(pos)
+    self.velocity=Vec2(0,0)
+    self.debug_visual=True
+    self.sprite=sprite
+
+  def gatherDrawCalls(self, pos=Vec2(0, 0), draw:list=[]):
+    draw.append(self.sprite.build(self.pos+pos))
+    return super().gatherDrawCalls(pos, draw)
+  
+class Drawable(GameObject):
+  def __init__(self, pos:Vec2):
+    super().__init__(pos)
+
+  def __call__(self,pos:Vec2):
+    self.pos=pos
+
+  def gatherDrawCalls(self, pos=Vec2(0, 0), draw=[]):
+    draw.append(Rectangle(pos))
+    return super().gatherDrawCalls(pos, draw)
+
+
+class ImageCompiler:
+  def __init__(self):
+    self.actions=[]
+  def draw(self,fb):
+    sortedActions=sorted(self.actions)
+    action:RenderAction
+    for action in sortedActions:
+      action.action(fb,Vec2(0,0))
+    self.actions.clear()
+
+def compile_and_draw(root:Node):
+  compiler=ImageCompiler()
+  root.gatherDrawCalls(draw=compiler.actions)
+  print(compiler.actions)
+  compiler.draw("")
+root=Node(Vec2(0,0))
+rectangle=GameObject(Vec2(0,0),Rectangle(Vec2(10,10),Vec2(5,5)))
+root.add_child(rectangle)
+compile_and_draw(root)
+print(rectangle.pos)
+rectangle.pos+=Vec2(5,5)
+print(rectangle.pos)
+compile_and_draw(rectangle)
